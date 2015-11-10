1feb95f4b4f63395f1099e6197df5d63fd1c45c1
benbynum <benbynum@gmail.com>
2015-11-10 13:10:35 -0500

    Add ProfileCtrl

---
+++ app/users/profile.controller.js
@@ -0,0 +1,8 @@
+angular.moduel('angularfireSlackApp')
+	.controller('ProfileCtrl', function($state, md5, auth, profile) {
+
+		// md5 - gravatar needs email to be md5 hashed
+		
+		var profileCtrl = this;
+
+	})
