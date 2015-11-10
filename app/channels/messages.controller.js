320f6f7f5bac873c650df374cfd32f97a1a37349
benbynum <benbynum@gmail.com>
2015-11-10 17:03:45 -0500

    Set messages and channelName on messagesCtrl to their respective dependencies

--- app/channels/messages.controller.js
+++ app/channels/messages.controller.js
@@ -1,4 +1,8 @@
 angular.module('angularfireSlackApp')
 	.controller('MessagesCtrl', function(profile, channelName, messages) {
 		var messagesCtrl = this;
+
+		// set messages and channel name to the respective dependencies
+		messagesCtrl.messages = messages;
+		messagesCtrl.channelName = channelName;
 	});
