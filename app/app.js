90b269521139cf7148f17b05a63adfc422261de6
benbynum <benbynum@gmail.com>
2015-11-10 12:57:16 -0500

    Add auth controller and state for profile

--- app/app.js
+++ app/app.js
@@ -22,13 +22,48 @@ angular
       })
       .state('login', {
         url: '/login',
-        templateUrl: 'auth/login.html'
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/login.html',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('home');
+            }, function(error){
+              return;
+            });
+          }
+        }
       })
       .state('register', {
         url: '/register',
-        templateUrl: 'auth/register.html'
+        controller: 'AuthCtrl as authCtrl',
+        templateUrl: 'auth/register.html',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('home');
+            }, function(error) {
+              return;
+            });
+          }
+        }
+      })
+      .state('profile', {
+        url: '/profile',
+        resolve: {
+          auth: function($state, Users, Auth) {
+            return Auth.$requireAuth().catch(function() {
+              $state.go('home');
+            });
+          },
+          profile: function(Users, Auth) {
+            return Auth.$requireAuth().then(function(auth){
+              return Users.getProfile(auth.uid).$loaded();
+            });
+          }
+        }
       });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://beroby-fireslack.firebaseio.com/');
+  .constant('FirebaseUrl', 'https://beroby-fireslack.firebase.io.com/');
