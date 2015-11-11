fa971d4204ca2b4a43c5642962f294e8af005caa
benbynum <benbynum@gmail.com>
2015-11-11 18:00:51 -0500

    Cleanup/comment global.directives.js

--- app/global/global.directives.js
+++ app/global/global.directives.js
@@ -2,69 +2,70 @@
 
 angular.module('app.directives', [])
 
+// Auto scroll to make most recent messages visible
+// Only scrolls when within x pixels (SCROLL_DIFF) from bottom
+// ie: If a user is trying to read old messages, don't auto scroll for each new msg
 .directive('scrollStick', function() {
   return {
   	restrict: 'A',
   	link: function(scope, element, attrs) {
-  		console.log('scroll sticking')
 
-  		// cache non jq lite element to use vanilla js scrollTop
+
+  		// Cache non jq lite element to use vanilla js scrollTop
   		var el = document.getElementsByClassName(element.attr('class'))[0];
-      console.log('el', el)
   		var scroll_pos = el.scrollTop;
-  		var li_height = 35; // height of message line to calculate scroll offset
-  		var scroll_height;
-  		var scroll_diff;
-  		var total_messages;
+  		var SCROLL_DIFF = 35;
+      // Distance from bottom that the scroll position needs to be in order to stick
+  		// Set `SCROLL_DIFF = 0` to auto scroll only when flush to the bottom
 
-  		// watch for changes
-  		// only update scroll if already at bottom
-  		// ie: if user wants to read previous messages, it's annoying to get bumped to the bottom every time there is a new one
+      var scroll_height;
+      var scroll_diff;
+
+      // Calculate current scroll position and diff from bottom
+      element.bind('scroll', function() {
+        scroll_pos = el.scrollTop;
+        scroll_diff = (el.scrollHeight - el.offsetHeight) - scroll_pos
+      })
+
+  		// Watch for new messages (ie: change in scroll height)
   		scope.$watch (function() { return el.scrollHeight; }, function(newVal, oldVal) {
-  			// window scrolled all the to bottom, or within li height of it, jump down for each new
-  			if ( scroll_diff <= 35 ) {
-  				el.scrollTop = el.scrollHeight;
-  			}
+  			if ( scroll_diff <= SCROLL_DIFF ) el.scrollTop = el.scrollHeight;
   		})
 
-  		element.bind('scroll', function() {
-  			scroll_pos = el.scrollTop;
-  			scroll_diff = (el.scrollHeight - el.offsetHeight) - scroll_pos
-  			console.log('scroll diff', scroll_diff)
-  			console.log('scroll pos: ', el.scrollTop)
-  			console.log('scroll height: ', el.scrollHeight - el.offsetHeight)
-  		})
 
   	}
   };
 })
+
+// Expand Mobile nav menu
 .directive('menuExpander', function() {
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
-      console.log('menu expander directive init')
 
+      // Cache selectors
       var sidebar = $('.sidebar')
 
+      // On click
       element.bind("click", function() {
-        console.log('nav clicked')
 
         var sidebarDisplay = sidebar.css('display')
 
-        // toggle display none of sidebar
-                console.log('sidebar, sidebarDisplay', sidebar, sidebarDisplay)
+        // Toggle display none of sidebar
         if (sidebarDisplay == 'none') {
           sidebar.css('display', 'inline-block')
         } else {
           sidebar.css('display', 'none')
         }
+
       })
 
+      // Hide sidebar on channel-list click if in mobile view
       $('.channel-list').bind("click", function() {
-        console.log('channel list clicked')
-
         if ( window.innerWidth < 768 ) sidebar.css('display', 'none')
       })
+
+      
     }
   }
 })
